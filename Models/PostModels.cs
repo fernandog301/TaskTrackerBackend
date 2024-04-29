a3f13e0b9f2439afc6fa9795067d7eb7aac1f533
JayvonK <jaykhuth@gmail.com>
2024-04-29 11:49:39 -0700

    create post endpoint

--- Models/PostModels.cs
+++ Models/PostModels.cs
@@ -8,19 +8,14 @@ namespace TaskTrackerBackend.Models
     public class PostModels
     {
         public int ID { get; set; }
-
         public int UserId { get; set; }
-
         public string Description { get; set; }
-
         public string Title { get; set; }
-
         public string DateCreated { get; set; }
-
-        public string AlignedTo { get; set; }
-
-        public string? DateUpdated { get; set; }
-
+        public int AssigneeID { get; set; }
+        public string Status { get; set; }
+        public string PriorityLevel { get; set; }
+        public List<CommentsModels> Comments { get; set; }
         public bool IsDeleted { get; set; } = false;
 
         public PostModels(){
--- Services/PostService.cs
+++ Services/PostService.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TaskTrackerBackend.Models;
+using TaskTrackerBackend.Models.DTO;
+using TaskTrackerBackend.Services.Context;
 
 namespace TaskTrackerBackend.Services
 {
     public class PostService : ControllerBase
     {
+        private readonly DataContext _context;
+        public PostService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public UserModels GetUserByUsername(string username) {
+            return _context.UserInfo.SingleOrDefault(user => user.Username == username);
+        }
+
+        public bool CreatePost(string username, CreatePostDTO createdPost) {
+            UserModels foundUser = GetUserByUsername(username);
+            UserModels assignee = GetUserByUsername(createdPost.Assignee);
+            var newPost = new PostModels();
+            newPost.UserId = foundUser.ID;
+            newPost.Description = createdPost.Description;
+            newPost.Title = createdPost.Title;
+            newPost.DateCreated = createdPost.DateCreated;
+            newPost.AssigneeID = assignee.ID;
+            newPost.Status = createdPost.Status;
+            newPost.PriorityLevel = createdPost.PriorityLevel;
+            newPost.Comments = new List<CommentsModels>();
+            newPost.IsDeleted = false;
+
+            return _context.SaveChanges() != 0;
+        }
+
         
     }
 }
