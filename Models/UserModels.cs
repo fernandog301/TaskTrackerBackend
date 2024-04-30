6495d05a9574704e7810f882113d177d8cae1127
JayvonK <jaykhuth@gmail.com>
2024-04-30 10:53:47 -0700

    headers out

--- Models/UserModels.cs
+++ Models/UserModels.cs
@@ -10,26 +10,14 @@ namespace TaskTrackerBackend.Models
         public int ID { get; set; }
 
         public string Username { get; set; }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         public string AccountCreated { get; set; }
 
-        
         public string Password { get; set; }
-=======
-        
->>>>>>> 68671510b6e5dca6dced2185bfea9b2c9df64832
-        
-=======
-        
-
->>>>>>> bc137981cea85b634c05ba70698df1bc0b516ae5
         public string? Salt { get; set; }
 
 
         public string? Hash { get; set; }
 
-        public UserModels(){}
+        public UserModels() { }
     }
 }
