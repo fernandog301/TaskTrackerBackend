998d8e4c690d9645d8660e312d5fccc3cf10b99d
fernandog301 <fernandog301g@gmail.com>
2024-04-30 10:51:09 -0700

    test

--- Models/UserModels.cs
+++ Models/UserModels.cs
@@ -11,20 +11,11 @@ namespace TaskTrackerBackend.Models
 
         public string Username { get; set; }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         public string AccountCreated { get; set; }
 
         
         public string Password { get; set; }
-=======
-        
->>>>>>> 68671510b6e5dca6dced2185bfea9b2c9df64832
-        
-=======
-        
-
->>>>>>> bc137981cea85b634c05ba70698df1bc0b516ae5
         public string? Salt { get; set; }
 
 
